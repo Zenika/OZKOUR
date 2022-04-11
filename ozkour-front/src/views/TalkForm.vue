3bb3e5e092494bbc0955f3910060a8fa15fedf82
BKsensei <rebecca.kaci@gmail.com>
2022-04-11 17:37:23 +0200

    Component ChoosingTemplate added in the TalkForm view

--- ozkour-front/src/views/TalkForm.vue
+++ ozkour-front/src/views/TalkForm.vue
@@ -1,17 +1,38 @@
 <script>
-import { defineComponent } from '@vue/composition-api'
+import ChoosingTemplate from '../components/ChoosingTemplate.vue'
 
-export default defineComponent({
-    setup() {
-        
-    },
-})
+export default {
+  components: {
+    ChoosingTemplate
+  },
+}
+
 </script>
 
+
 <template>
-    
+    <main class="sections">
+        <h1>VISUELS TALK</h1>
+
+        <section class="section">
+            <ChoosingTemplate />
+        </section>
+        
+        <section class="section">
+            <h2>Liste des événements sélectionnés</h2>
+        </section>
+
+        <section class="section">
+            <button>VALIDER</button>
+            <button>GENERER UN VISUEL</button>
+        </section>
+        
+    </main> 
 </template>
 
 <style scoped>
-
+.sections {
+    display: flex;
+    flex-direction: column;
+}
 </style>
