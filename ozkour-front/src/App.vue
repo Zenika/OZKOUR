a34cfee0da2a24a3c95806fa2ff30d5bbe0f1e23
BKsensei <rebecca.kaci@gmail.com>
2022-04-12 10:29:52 +0200

    dark background + white text color + font-family added

--- ozkour-front/src/App.vue
+++ ozkour-front/src/App.vue
@@ -15,12 +15,14 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #1E1E1E;
+  color: #FFFFFF;
+  font-family: 'Open Sans', sans-serif;
 }
 </style>
