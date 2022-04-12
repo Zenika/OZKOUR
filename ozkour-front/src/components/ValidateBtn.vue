8dcacac084e73f94e92aed8f3cdd0ce335fe4131
BKsensei <rebecca.kaci@gmail.com>
2022-04-12 14:15:23 +0200

    css style added to Validate and Generate Buttons components

--- ozkour-front/src/components/ValidateBtn.vue
+++ ozkour-front/src/components/ValidateBtn.vue
@@ -3,9 +3,18 @@
 </script>
 
 <template>
-    <button>VALIDER</button>
+    <button class="validate-btn">VALIDER</button>
 </template>
 
 <style scoped>
-
+.validate-btn{
+    width: 15%;
+    background-color: #FFFFFF;
+    color: #BF1D67;
+    font-weight: 800;
+    border: none;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 30px;
+    padding: 15px;
+}
 </style>
