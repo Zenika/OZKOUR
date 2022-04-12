985aa0259c2142d44e75cd79b2bae14570e66f22
BKsensei <rebecca.kaci@gmail.com>
2022-04-12 16:11:54 +0200

    css style updated in the ChoosingTemplate component

--- ozkour-front/src/components/ChoosingTemplate.vue
+++ ozkour-front/src/components/ChoosingTemplate.vue
@@ -2,20 +2,20 @@
     <main class="flex-column">
         <h3>Choisir un visuel</h3>
         <div class="flex-start">
-            <div>
-                <input type="radio" id="quoide9" name="template" value="quoide9">
+            <div class="templateChoice">
+                <input type="radio" id="quoide9" name="template" value="quoide9" class="radio-btn">
                 <label for="quoide9">QUOI DE 9</label>
             </div>
             <div>
-                <input type="radio" id="emailing" name="template" value="emailing">
+                <input type="radio" id="emailing" name="template" value="emailing" class="radio-btn">
                 <label for="emailing">E-MAILING</label>
             </div>
             <div>
-                <input type="radio" id="meetup" name="template" value="meetup">
+                <input type="radio" id="meetup" name="template" value="meetup" class="radio-btn">
                 <label for="meetup">MEET-UP</label>
             </div>
             <div>
-                <input type="radio" id="slack" name="template" value="slack">
+                <input type="radio" id="slack" name="template" value="slack" class="radio-btn">
                 <label for="slack">SLACK</label>
             </div>
         </div>
@@ -28,8 +28,20 @@
     flex-direction: column;
 }
 .flex-start {
+    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+    gap: 10px;
+    font-size: 16px;
+    letter-spacing: .0.5rem;
+}
+
+.radio-btn {
+    cursor: pointer;
+}
+
+label {
+    padding-left: 10px;
 }
 </style>
