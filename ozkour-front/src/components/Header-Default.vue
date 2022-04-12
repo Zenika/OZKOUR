e682f7c75faa07324e407d87b42315537b2a7b59
Pranxtor <pranxtor02@gmail.com>
2022-04-12 12:47:48 +0200

    css header update links

--- ozkour-front/src/components/Header-Default.vue
+++ ozkour-front/src/components/Header-Default.vue
@@ -1,35 +1,37 @@
 <template>
     <header>
         <div id='header'>
-            <a href="/" ><img id="logoZenika" alt="Zenika logo" src="../assets/Logo_Horizontal_Blanc.png"></a>
-            
+            <div id='header-logo'>
+                <a href="/" tabindex="1">
+                    <img id="logoZenika" alt="Zenika logo" src="../assets/Logo_Horizontal_Blanc.png" >
+                </a>      
+            </div>  
             <div id='header-user'>
                 <span>Nom Prénom</span>
-            
-                <img id="imgLogout" alt="image logout" src="../assets/images/logout.png">
+                <img id="imgLogout" @keyup.enter="test" @click="test" alt="image logout" src="../assets/images/logout.png" tabindex="2">
             </div>
-            
-            
         </div>
     </header>
 </template>
 
+<script setup>
+    function test(){
+        console.log("test")
+    }
+</script>
+
 <style scoped>
     #logoZenika{
-        width: 15%;
-        margin: 2%;
-        float: left;
+        width: 100%;
     }
     #imgLogout{
         width: 10%;
-        margin: 2%;
-        right:0px;
+        cursor: pointer;
     }
     #header{
-        text-align: right;
         width:100%;
         display:flex;
-        justify-content: space-around;
+        justify-content: space-between;
     }   
     #header-user{
         height:100px;
@@ -38,6 +40,15 @@
         display: flex;
         justify-content: flex-end;
         align-items: center;
+        margin-right: 2%;
+        gap:5%;
+    }
+    div#header-logo{
+        width: 15%;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        margin-left: 2%;
     }
     header{
         margin-bottom: 5%;
@@ -52,12 +63,7 @@
         font-family: 'OpenSans-ExtraBold';
         display: inline-block;
     }
-    table{
-        text-align: right;
-        width:auto;
-        display: inline;
-    }
-    td{
-        text-align: right;
-    }
+    /* a{
+        border:1px solid red;  
+    } */
 </style>
