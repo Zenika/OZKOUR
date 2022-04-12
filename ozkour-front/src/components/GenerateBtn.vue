f8dfeb057fd769add730cd26fd63011fdb3dc99d
BKsensei <rebecca.kaci@gmail.com>
2022-04-12 14:36:26 +0200

    pointer cursor added to the Validate and Generate buttons

--- ozkour-front/src/components/GenerateBtn.vue
+++ ozkour-front/src/components/GenerateBtn.vue
@@ -17,6 +17,7 @@
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 30px;
     padding: 15px;
+    cursor: pointer;
 }
 
 </style>
--- ozkour-front/src/components/ValidateBtn.vue
+++ ozkour-front/src/components/ValidateBtn.vue
@@ -16,5 +16,6 @@
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 30px;
     padding: 15px;
+    cursor: pointer;
 }
 </style>
