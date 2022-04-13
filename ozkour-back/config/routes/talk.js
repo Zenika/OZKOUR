ef1867f6cb7e83b50fe2bd0a4d32ceb1f5601fcf
Pranxtor <pranxtor02@gmail.com>
2022-04-13 14:19:53 +0200

    back | retrieve data from google sheets with date

--- ozkour-back/config/routes/talk.js
+++ ozkour-back/config/routes/talk.js
@@ -1,4 +1,24 @@
+const { getTalkFromDate } = require("../../google-api/sheets");
+//const sheets = require('../google-api/sheets')
+function test(request){
+    console.log(request)
+}
+
 module.exports = [
-    { method: 'GET', path: '/talk', handler: function () {} },
+    {
+     method: 'GET',
+     path: '/talk',
+     handler: function (request, h) {
+        return getTalkFromDate(request.query.start,request.query.end);
+    }},
+    // {method: 'POST',
+    // path: '/talks',
+    // handler: function (request, h) {
+
+    //     const payload = request.payload;
+
+    //     return `Welcome ${payload.username}!`;
+    // }},
+    
     //{ method: 'GET', path: '/users/{id}', handler: function () {} }
 ];
--- ozkour-back/index.js
+++ ozkour-back/index.js
@@ -4,17 +4,34 @@ const Hapi = require('@hapi/hapi');
 const connect = require('./google-api/connect.js');
 const sheets = require('./google-api/sheets.js');
 const routes = require('./config/routes');
+const Qs = require('qs');
 
 const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: 'localhost',
+        query: {
+            parser: (query) => Qs.parse(query)
+        }
     });
 
     connect.auth();
 
+
     server.route(routes);
+
+    // server.route({
+    //     method: 'POST',
+    //     path: '/signup',
+    //     handler: function (request, h) {
+    
+    //         const payload = request.payload;
+    
+    //         return `Welcome ${payload.username}!`;
+    //     }
+    // });
+
 
     // server.route({
     //     method: 'GET',
@@ -24,7 +41,7 @@ const init = async () => {
     //         return 'Hello World!';
     //     }
     // });
-    sheets.getTalkFromDate();
+    sheets.getTalkFromDate('18/01/2022');
     await server.start();
     //console.log('Server running on %s', server.info.uri);
 };
