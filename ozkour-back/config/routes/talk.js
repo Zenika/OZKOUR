4b85d32adcb0c5a6f6b4d772f70087f841234587
BKsensei <rebecca.kaci@gmail.com>
2022-05-05 11:51:45 +0200

    ajout de la route POST pour récupérée les talks sélectionnés dans le front

--- ozkour-back/config/routes/talk.js
+++ ozkour-back/config/routes/talk.js
@@ -1,18 +1,26 @@
+
 const { getTalkFromDate } = require("../../google-api/sheets");
 
 module.exports = [
     {
-    config: {
-        cors: {
-            origin: ['*'],
-            additionalHeaders: ['cache-control', 'x-requested-with']
-        }
-    },
-     method: 'GET',
-     path: '/talk',
-     handler: function (request, h) {
+    method: 'GET',
+    path: '/talk',
+    handler: function (request, h) {
         return getTalkFromDate(request.query.start,request.query.end);
     }},
-    
     //{ method: 'GET', path: '/talk/{id}', handler: function () {} }
+    {
+        method: 'POST',
+        path: '/selected-talks',
+        handler: function (request, h) {
+            return {
+                date: request.payload.date,    
+                universe: request.payload.universe,
+                eventType: request.payload.eventType,
+                eventName: request.payload.eventName,
+                talkTitle: request.payload.talkTitle,
+                speakers: request.payload.speakers,
+            }
+        },
+    }
 ];
