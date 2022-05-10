a7a2566350215a5ee7cd38d558872418e7fd1065
BKsensei <rebecca.kaci@gmail.com>
2022-05-10 10:46:02 +0200

    Validate Button css style updated

--- ozkour-front/src/components/Buttons/ValidateBtn.vue
+++ ozkour-front/src/components/Buttons/ValidateBtn.vue
@@ -8,18 +8,20 @@
 
 <style scoped>
     .validate-btn{
-        width: 15%;
-        background-color: #FFFFFF;
-        color: #BF1D67;
+        /* width: 15%; */
+        background: linear-gradient(90deg, #EE2238 0%, #C21E65 100%);
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        color: #FFFFFF;
         font-size: 18px;
         font-weight: 800;
         border: none;
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
         border-radius: 30px;
-        padding: 15px;
+        padding: 10px 30px;
         cursor: pointer;
     }
     .validate-btn:hover {
-        background: #F2F2F2;;
+        background: black;
+        color: white;
     }
 </style>
