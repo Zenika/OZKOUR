71befbabf2f5622ae134b457a326cead09e1bad1
Pranxtor <pranxtor02@gmail.com>
2022-05-18 12:14:38 +0200

    :sparkles: sort table talk

--- ozkour-front/src/components/EventArray.vue
+++ ozkour-front/src/components/EventArray.vue
@@ -6,14 +6,80 @@
       <table>
         <tr>
           <th scope="col"></th>
-          <th scope="col">DATE</th>
-          <th scope="col">UNIVERS</th>
-          <th scope="col">TYPE</th>
-          <th scope="col">NOM DE L'EVENEMENT</th>
-          <th scope="col">TITRE DU TALK</th>
-          <th scope="col">SPEAKER</th>
+          <th
+            scope="col"
+            :class="{ columnSelected: sort === 'date', selectable: true }"
+            v-on:click="setSort('date')"
+          >
+            DATE
+            <div
+              class="arrow"
+              v-if="sort === 'date'"
+              v-bind:class="ascending ? 'arrow_up' : 'arrow_down'"
+            ></div>
+          </th>
+          <th
+            scope="col"
+            :class="{ columnSelected: sort === 'universe', selectable: true }"
+            v-on:click="setSort('universe')"
+          >
+            UNIVERS
+            <div
+              class="arrow"
+              v-if="sort === 'universe'"
+              v-bind:class="ascending ? 'arrow_up' : 'arrow_down'"
+            ></div>
+          </th>
+          <th
+            scope="col"
+            :class="{ columnSelected: sort === 'eventType', selectable: true }"
+            v-on:click="setSort('eventType')"
+          >
+            TYPE
+            <div
+              class="arrow"
+              v-if="sort === 'eventType'"
+              v-bind:class="ascending ? 'arrow_up' : 'arrow_down'"
+            ></div>
+          </th>
+          <th
+            scope="col"
+            :class="{ columnSelected: sort === 'eventName', selectable: true }"
+            v-on:click="setSort('eventName')"
+          >
+            NOM DE L'EVENEMENT
+            <div
+              class="arrow"
+              v-if="sort === 'eventName'"
+              v-bind:class="ascending ? 'arrow_up' : 'arrow_down'"
+            ></div>
+          </th>
+          <th
+            scope="col"
+            :class="{ columnSelected: sort === 'talkTitle', selectable: true }"
+            v-on:click="setSort('talkTitle')"
+          >
+            TITRE DU TALK
+            <div
+              class="arrow"
+              v-if="sort === 'talkTitle'"
+              v-bind:class="ascending ? 'arrow_up' : 'arrow_down'"
+            ></div>
+          </th>
+          <th
+            scope="col"
+            :class="{ columnSelected: sort === 'speakers', selectable: true }"
+            v-on:click="setSort('speakers')"
+          >
+            SPEAKER
+            <div
+              class="arrow"
+              v-if="sort === 'speakers'"
+              v-bind:class="ascending ? 'arrow_up' : 'arrow_down'"
+            ></div>
+          </th>
         </tr>
-        <tr v-for="talk in talk.retrieved" v-bind:key="talk" data-test="talks">
+        <tr v-for="talk in sortedTalk" v-bind:key="talk" data-test="talks">
           <td>
             <input
               type="checkbox"
@@ -37,11 +103,34 @@
 </template>
 
 <script setup>
-//import { ref } from 'vue'
+import { ref } from "vue";
+import { computed } from "@vue/runtime-core";
 import { useTalkStore } from "../stores/talks";
 
 const talk = useTalkStore();
 
+const sort = ref("");
+const ascending = ref(false);
+
+function compare(a, b) {
+  if (a[sort.value] < b[sort.value]) return -1;
+  if (a[sort.value] > b[sort.value]) return 1;
+  // a doit être égal à b
+  return 0;
+}
+
+const sortedTalk = computed(() => {
+  if (ascending.value) return talk.retrieved.sort((a, b) => compare(a, b));
+  else return talk.retrieved.sort((a, b) => compare(b, a));
+});
+
+function setSort(column) {
+  if (column === sort.value) ascending.value = !ascending.value;
+  else {
+    ascending.value = true;
+    sort.value = column;
+  }
+}
 
 //console.log(talk)
 
@@ -50,7 +139,7 @@ function check(talkSelected, event) {
     date: talkSelected.date,
     universe: talkSelected.universe,
     eventType: talkSelected.eventType,
-    eventName: talkSelected.eventName ,
+    eventName: talkSelected.eventName,
     talkTitle: talkSelected.talkTitle,
     speakers: talkSelected.speakers,
     checked: talkSelected.checked,
@@ -101,6 +190,31 @@ td {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.columnSelected {
+  text-decoration: underline;
+}
+
+.selectable {
+  cursor: pointer;
+}
+
+.arrow_down {
+  background-image: url("../assets/images/arrow-down-1.png");
+}
+
+.arrow_up {
+  background-image: url("../assets/images/arrow-up-2.png");
+}
+
+.arrow {
+  float: right;
+  width: 12px;
+  height: 15px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position-y: bottom;
 }
 
 .detail {
