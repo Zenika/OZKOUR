da4c616e23c3b7b3d4f79c0fa5238928001558ff
Pranxtor <pranxtor02@gmail.com>
2022-05-30 15:44:03 +0200

    add test back utilitary and sheets

--- ozkour-back/google-api/sheets.js
+++ ozkour-back/google-api/sheets.js
@@ -46,7 +46,7 @@ async function getData(auth, params) {
   const res = await sheets.spreadsheets.values.get({
     spreadsheetId: "1e50uVl_wAseWD8PDyAeNS9dRNhiq0k_WVyJr2fL9SeE", // TO DO use a variable instead of a link
     range:`${utilitary.convDateToMonth(params.start)} ${dayjs(params.start, 'DD/MM/YYYY').format('YYYY')}!A2:H`,
-    //range: "Janvier 2021!A2:H",
+    
   });
   return dateFilter(res.data.values, params.start, params.end);
 }
@@ -54,19 +54,20 @@ async function getData(auth, params) {
 /**
  * Filter the talks between 2 dates
  * @param {String} talks the talks that need to be filtered
- * @param {String} start the start of the date range
- * @param {String} end the end of the date range
+ * @param {String} start the start of the date range (format='DD-MM-YYYY')
+ * @param {String} end the end of the date range (format='DD-MM-YYYY')
  */
 function dateFilter(talks, start, end) {
   const formatedDateStart = dayjs(start, 'DD-MM-YYYY');
   const formatedDateEnd = dayjs(end, 'DD-MM-YYYY');
   talks = talks.filter(function (talk) {
     const dateTalk = dayjs(talk[4], 'DD-MM-YYYY');
-    return formatedDateStart.isBefore(dateTalk,'day') && formatedDateEnd.isAfter(dateTalk,'day');
+    return formatedDateStart.isBefore(dateTalk.add(1,'day'),'day') && formatedDateEnd.isAfter(dateTalk.add(-1,'day'),'day');
   });
   return talks;
 }
 
 module.exports = {
   getTalkFromDate,
+  dateFilter
 };
