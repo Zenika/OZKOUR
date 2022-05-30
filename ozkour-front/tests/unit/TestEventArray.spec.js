370518f1e5d711b359346d33329482a6e6e5d9b6
Pranxtor <pranxtor02@gmail.com>
2022-05-30 10:32:24 +0200

    test EventArray Completed

--- ozkour-front/tests/unit/TestEventArray.spec.js
+++ ozkour-front/tests/unit/TestEventArray.spec.js
@@ -3,6 +3,17 @@ import EventArray from "@/components/EventArray.vue";
 import { createTestingPinia } from "@pinia/testing";
 import { useTalkStore } from "../../src/stores/talks";
 
+function isAscending(arr) {
+  return arr.every(function (x, i) {
+    return i === 0 || x >= arr[i - 1];
+  });
+}
+function isDescending(arr) {
+  return arr.every(function (x, i) {
+    return i === 0 || x <= arr[i - 1];
+  });
+}
+
 describe("ListEvent Component", () => {
   it("Display title when no talk", () => {
     // Create an instance of our component
@@ -70,7 +81,7 @@ describe("ListEvent Component", () => {
           plugins: [
             createTestingPinia({
               initialState: {
-                talk: { retrieved: talksRetrieved},
+                talk: { retrieved: talksRetrieved },
               },
             }),
           ],
@@ -78,12 +89,10 @@ describe("ListEvent Component", () => {
       });
 
       const talk = useTalkStore();
-
       const inputCheckbox = wrapper.find('[type="checkbox"]');
-
       await inputCheckbox.setChecked(false);
 
-      expect(inputCheckbox.element.checked).toBe(false);      
+      expect(inputCheckbox.element.checked).toBe(false);
       expect(talk.uncheckTalk).toHaveBeenCalledTimes(1);
     }),
     it("check talk is working", async () => {
@@ -93,7 +102,30 @@ describe("ListEvent Component", () => {
           plugins: [
             createTestingPinia({
               initialState: {
-                talk: { retrieved: talksRetrieved},
+                talk: { retrieved: talksRetrieved },
+              },
+            }),
+          ],
+        },
+      });
+
+      const talk = useTalkStore();
+      const inputCheckbox = wrapper.find('[type="checkbox"]');
+
+      await inputCheckbox.setChecked(false);
+      await inputCheckbox.setChecked(true);
+
+      expect(inputCheckbox.element.checked).toBe(true);
+      expect(talk.checkTalk).toHaveBeenCalledTimes(1);
+    }),
+    it("check order(ascending/discending) when clicking on column", async () => {
+      // Create an instance of our component
+      const wrapper = mount(EventArray, {
+        global: {
+          plugins: [
+            createTestingPinia({
+              initialState: {
+                talk: { retrieved: talksRetrieved },
               },
             }),
           ],
@@ -102,14 +134,32 @@ describe("ListEvent Component", () => {
 
       const talk = useTalkStore();
 
-      const inputCheckbox = wrapper.find('[type="checkbox"]');
+      let columns = wrapper.find('[data-test="speakers"]');
+      let talksLines = wrapper.findAll('[data-test="talks"]');
+      let listSpeakersOrderedAsTheyAppear = [];
 
-      await inputCheckbox.setChecked(false);
-      await inputCheckbox.setChecked(true);
+      await columns.trigger("click");
 
-      expect(inputCheckbox.element.checked).toBe(true);
-      expect(talk.checkTalk).toHaveBeenCalledTimes(1);
-    })
+      talksLines = wrapper.findAll('[data-test="talks"]');//reload talks
+
+      talksLines.forEach((element) => {
+        listSpeakersOrderedAsTheyAppear.push(element.findAll("td")[6].text());
+      });
+
+      expect(isAscending(listSpeakersOrderedAsTheyAppear)).toBe(true);
+
+      await columns.trigger("click");
+
+      talksLines = wrapper.findAll('[data-test="talks"]');//reload talks
+      listSpeakersOrderedAsTheyAppear = [];
+
+      talksLines.forEach((element) => {
+        listSpeakersOrderedAsTheyAppear.push(element.findAll("td")[6].text());
+      });
+
+      expect(isDescending(listSpeakersOrderedAsTheyAppear)).toBe(true);
+
+    });
 });
 
 const talksRetrieved = [
@@ -120,7 +170,7 @@ const talksRetrieved = [
     eventName: "GraalVM Night",
     talkTitle: "GraalVM for Sustainable Software Development?",
     speakers: "Adrien Nortain",
-    checked: true
+    checked: true,
   },
   {
     date: "19/01/2021",
@@ -129,7 +179,7 @@ const talksRetrieved = [
     eventName: "NightClass",
     talkTitle: "Migration JS vers TS sur du react",
     speakers: "Jules Hablot",
-    checked: true
+    checked: true,
   },
   {
     date: "21/01/2021",
@@ -138,7 +188,7 @@ const talksRetrieved = [
     eventName: "Nantes JS #55",
     talkTitle: "Nuxt 2021",
     speakers: "Yann Bertrand",
-    checked: true
+    checked: true,
   },
   {
     date: "21/01/2021",
@@ -147,7 +197,7 @@ const talksRetrieved = [
     eventName: "Webinar Strigo",
     talkTitle: "Simplify Remote Hands-On Training and Improve Engagement",
     speakers: "Yoan Rousseau / Oliver Huber",
-    checked: true
+    checked: true,
   },
   {
     date: "25/01/2021",
@@ -156,6 +206,6 @@ const talksRetrieved = [
     eventName: "RemoteClazz Nodejs",
     talkTitle: "Techniques minimalistes pour Node.js",
     speakers: "Hugo Wood",
-    checked: true
+    checked: true,
   },
 ];
