2f9d4d7b258894d16a41267a1212fbe4c9da4d83
BKsensei <rebecca.kaci@gmail.com>
2022-07-07 12:07:50 +0200

    integration test updated

--- ozkour-back/google-api/slideWrapper.js
+++ ozkour-back/google-api/slideWrapper.js
@@ -2,7 +2,7 @@ const { google } = require('googleapis')
 const connect = require('./connect.js')
 const util = require('util')
 
-const presentationId = '1Mwzl0-13stcTZRn_0iyIJLZveuY80SW2cmv9p2Wgpug'
+const presentationId = process.env.GOOGLE_SLIDE_LINK
 
 async function getSlides () {
   const auth = await connect.getAuthentication()
