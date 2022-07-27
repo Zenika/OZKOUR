3868359497e3710a0e6679a525fa4aad9a731904
Pranxtor <pranxtor02@gmail.com>
2022-07-27 10:12:21 +0200

    :white_check_mark: function isYearBetween2Dates + tests added

--- ozkour-back/Utils/dateUtils.js
+++ ozkour-back/Utils/dateUtils.js
@@ -1,16 +1,22 @@
 const dayjs = require('dayjs')
 require('dayjs/locale/fr')
 const customParseFormat = require('dayjs/plugin/customParseFormat')
+const isBetween = require('dayjs/plugin/isBetween')
+dayjs.extend(isBetween)
+dayjs.extend(customParseFormat)
 
 function convDateToMonthInLetter (date) {
   // the format used in the the google sheets is invalid with dayjs
   // format of date used in google sheets : 'DD/MM/YYYY'
   // format of date expected by dayjs : 'YYYY/MM/DD'
-  dayjs.extend(customParseFormat)
   const formatedDate = dayjs(date, 'DD-MM-YYYY', 'fr').format('MMMM')
 
   // the month is in uppercase in the googlesheet
   return formatedDate
+}
+
+function isYearBetweenDates (year, start, end) {
+  return dayjs(year).isBetween(dayjs(start), dayjs(end), 'year', '[]')
 }
 
 function displayFullDateWithWords (date) {
@@ -20,5 +26,6 @@ function displayFullDateWithWords (date) {
 
 module.exports = {
   displayFullDateWithWords,
-  convDateToMonthInLetter
+  convDateToMonthInLetter,
+  isYearBetweenDates
 }
--- ozkour-back/tests/dateUtils.test.js
+++ ozkour-back/tests/dateUtils.test.js
@@ -1,5 +1,19 @@
 const dateUtils = require('../Utils/dateUtils')
 describe('dateUtils', () => {
+  describe('Test on date', () => {
+    it('should return true when the year is between two dates', () => {
+      // lower bound
+      expect(dateUtils.isYearBetweenDates('2021', '2021-03-21', '2022-03-21')).toBeTruthy()
+      // upper bound
+      expect(dateUtils.isYearBetweenDates('2022', '2021-03-21', '2022-03-21')).toBeTruthy()
+    })
+    it('should return false when the year is not between two dates', () => {
+      // lower bound
+      expect(dateUtils.isYearBetweenDates('2020', '2021-03-21', '2022-03-21')).toBeFalsy()
+      // upper bound
+      expect(dateUtils.isYearBetweenDates('2023', '2021-03-21', '2022-03-21')).toBeFalsy()
+    })
+  })
   describe('Display date as words', () => {
     it('should return a month as words, given a month in number format', () => {
       expect(dateUtils.convDateToMonthInLetter('11/03/2021')).toBe('mars')
