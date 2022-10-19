55ab16580caa0698ccdda83af44b3cf7052951f1
Pranxtor <pranxtor02@gmail.com>
2022-10-19 16:10:13 +0200

    :coffin:remove unused css on Popup and Recap modal

--- ozkour-front/src/components/PopUp.vue
+++ ozkour-front/src/components/PopUp.vue
@@ -33,9 +33,9 @@ export default {
       <h2>{{ title }}</h2>
     </div>
 
-    <div class="recap">
-      <div class="recap-details" />
-      <p class="text-pop-up">
+    <div class="message">
+      <div class="message-details" />
+      <p class="message-pop-up">
         {{ message }}
       </p>
     </div>
@@ -69,14 +69,6 @@ export default {
   padding: 0px 20px;
 }
 
-.icon-bg {
-  @include popup-icon-bg;
-}
-
-.icon {
-  width: 20px;
-}
-
 .recap-details {
   @include popup-summary;
 }
@@ -85,56 +77,10 @@ export default {
   text-align: center;
 }
 
-.events {
-  margin: 0px;
-  padding-left: 60px;
-}
-
 .validate {
   display: flex;
   justify-content: center;
   padding: 30px 0px;
 }
 
-.loading-container {
-  display:grid;
-  justify-content:center;
-  position:fixed;
-  left:48.5%;
-  top:50%;
-}
-
-.loading {
-  border-radius:50%;
-  width:2.5em;
-  height:2.5em;
-  transform-origin:center;
-  animation: rotate 1s linear infinite;
-}
-
-.spinner {
-  width: 7em;
-  height: 7em;
-  left: -2.8em;
-  top: -2.8em;
-  border-top: 1em solid #C21E65;
-  position:relative;
-  border-right: 1em solid transparent;
-  border-radius: 50%;
-}
-
-.head {
-  width: 1em;
-  height: 1em;
-  border-radius: 50%;
-  margin-left: 5.9em;
-  margin-top: -0.05em;
-  background: linear-gradient(-25deg, #EE2238 0%, #C21E65 100%);
-}
-
-@keyframes rotate{
-  to{
-    transform:rotate(360deg);
-  }
-}
 </style>
