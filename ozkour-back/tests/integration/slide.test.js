a92ef2faf199d5dc644719c00161df0ec1047caf
Pranxtor <pranxtor02@gmail.com>
2022-10-24 15:44:16 +0200

    :white_check_mark: update integration test to delete the slide even if the test fails

--- ozkour-back/tests/integration/slide.test.js
+++ ozkour-back/tests/integration/slide.test.js
@@ -6,34 +6,38 @@ const connect = require('../../src/infrastructure/connect.js')
 const slideServiceRepository = googleSlideRepository
 const slideService = new SlideService(slideServiceRepository)
 
-describe('Integration test on create a slide', () => {
-  it('should generate expected Google Slide file', async () => {
+describe('Integration test slides', () => {
+  describe('when creating a slide based on the template', () => {
+    afterEach(async () => {
+      await slideService.deleteLastSlide()
+    })
+
+    it('should generate expected Google Slide file', async () => {
     // given
-    const talks = [_createValidTalk()]
-    try {
+      const talks = [_createValidTalk()]
+      try {
       // when
-      connect.createAuth()
-      const res = await slideService.createSlides(talks)
-      const slides = await getSlides()
-      // then
-      expect(res).toStrictEqual({
-        message: 'Created !',
-        link: 'https://docs.google.com/presentation/d/' +
+        connect.createAuth()
+        const res = await slideService.createSlides(talks)
+        const slides = await getSlides()
+        // then
+        expect(res).toStrictEqual({
+          message: 'Created !',
+          link: 'https://docs.google.com/presentation/d/' +
             presentationId +
             '/'
-      })
-      expect(JSON.stringify(slides[1])
-        .replace(/"objectId":".*?",/g, '"objectId":"id",')
-        .replace(/"speakerNotesObjectId":".*?"/g, '"speakerNotesObjectId":"id"')
-        .replace(/"https:\/\/lh[1-9].googleusercontent.com\/.*?",/g, '"lien",')
-        .replace(/"listId":".*?"/g, '"listId":"listId"')
-        .replace(/"lists":{".*?"/g, '"lists":"lists"')).toMatchSnapshot()
-
-      await slideService.deleteLastSlide()
-    } catch (e) {
-      console.error(e)
-      throw e
-    }
+        })
+        expect(JSON.stringify(slides[1])
+          .replace(/"objectId":".*?",/g, '"objectId":"id",')
+          .replace(/"speakerNotesObjectId":".*?"/g, '"speakerNotesObjectId":"id"')
+          .replace(/"https:\/\/lh[1-9].googleusercontent.com\/.*?",/g, '"lien",')
+          .replace(/"listId":".*?"/g, '"listId":"listId"')
+          .replace(/"lists":{".*?"/g, '"lists":"lists"')).toMatchSnapshot()
+      } catch (e) {
+        console.error(e)
+        throw e
+      }
+    })
   })
 })
 
