aa5c857c6d792b1a9e268ed8a70c5793a53fbb72
SamyBENYOUB <sbenyoub@figarocms.fr>
2023-06-13 12:42:39 +0200

    168: google-auth-garder-la-session-de-lutilisateur-au-refresh

--- ozkour-front/src/main.js
+++ ozkour-front/src/main.js
@@ -11,7 +11,9 @@ app.use(
   createAuth0({
     domain: process.env.VUE_APP_AUTH0_DOMAIN,
     client_id: process.env.VUE_APP_AUTH0_CLIENT_ID,
-    redirect_uri: window.location.origin
+    redirect_uri: window.location.origin,
+    useRefreshTokens: true,
+    cacheLocation: 'localstorage'
   })
 )
 
