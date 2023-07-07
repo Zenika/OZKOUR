aa6d023e04afc1e4980536b2cdddb471cd0476c3
edouardglx <edouard.gloux@zenika.com>
2023-07-07 16:29:55 +0200

    added testing for Warning component and Stores

--- ozkour-front/tests/unit/dataset/trainings.dataset.js
+++ ozkour-front/tests/unit/dataset/trainings.dataset.js
@@ -1,50 +1,57 @@
-export function dataSetFactory () {
+export function dataSetFactory() {
   const trainings = {
     springCore: {
-      date: '19/01/2021',
-      trainingTitle: 'Spring Core',
-      universe: 'Architecture, Langages & Algo',
-      duration: '4',
-      price: '2095',
-      link: 'https://training.zenika.com/fr-fr/training/spring/description',
-      checked: true
+      date: "19/01/2021",
+      title: "Spring Core",
+      universe: "Architecture, Langages & Algo",
+      duration: "4",
+      price: "2095€",
+      url: "https://training.zenika.com/fr-fr/training/spring/description",
+      checked: true,
+      indexLine: 8,
     },
     kafkaConfluent: {
-      date: '19/01/2021',
-      trainingTitle: 'Kafka Streams & Confluent KSQL',
-      universe: 'Architecture, Big Data & Data Science',
-      duration: '3',
-      price: '2100',
-      link: 'https://training.zenika.com/fr-fr/training/kafka-ksql/description',
-      checked: true
-    }
-  }
-  const allTrainings = [
-    trainings.springCore,
-    trainings.kafkaConfluent
-  ]
+      date: "19/01/2021",
+      title: "Kafka Streams & Confluent KSQL",
+      universe: "Architecture, Big Data & Data Science",
+      duration: "3",
+      price: "2100€",
+      url: "https://training.zenika.com/fr-fr/training/kafka-ksql/description",
+      checked: true,
+      indexLine: 12,
+    },
+  };
+  const allTrainings = [trainings.springCore, trainings.kafkaConfluent];
+
+  const allTrainingsWithPartialDatas = {
+    res: [trainings.springCore, trainings.kafkaConfluent],
+    warn: [2, 3],
+  };
+
   const templates = {
     EMAILING: {
-      id: 'emailing',
-      label: 'E-MAILING',
-      value: 'E-mailing',
-      frequency: 'month',
-      validated: true
+      id: "emailing",
+      label: "E-MAILING",
+      value: "E-mailing",
+      frequency: "month",
+      validated: true,
     },
-    SLIDE:
-    {
-      id: 'slide',
-      label: 'SLIDE',
-      value: 'Slide',
-      frequency: 'month',
-      validated: false
-    }
-  }
+    SLIDE: {
+      id: "slide",
+      label: "SLIDE",
+      value: "Slide",
+      frequency: "month",
+      validated: false,
+    },
+  };
 
-  const allTemplates = [
-    templates.EMAILING,
-    templates.SLIDE
-  ]
+  const allTemplates = [templates.EMAILING, templates.SLIDE];
 
-  return { trainings, allTrainings, templates, allTemplates }
+  return {
+    trainings,
+    allTrainings,
+    templates,
+    allTemplates,
+    allTrainingsWithPartialDatas,
+  };
 }
